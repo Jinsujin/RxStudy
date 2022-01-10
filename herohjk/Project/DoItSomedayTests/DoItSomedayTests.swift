6a5ce7a13b8aed5e15209dcabd9605fb0eed8a3a
김호종 <herohjk@naver.com>
2022-01-10 19:17:44 +0900

    .

---
+++ herohjk/Project/DoItSomedayTests/DoItSomedayTests.swift
@@ -0,0 +1,24 @@
+//
+//  DoItSomedayTests.swift
+//  DoItSomedayTests
+//
+//  Created by 김호종 on 2022/01/10.
+//
+
+import XCTest
+@testable import DoItSomeday
+// swiftlint:disable overridden_super_call
+class DoItSomedayTests: XCTestCase {
+
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws { }
+
+    func testExample() throws { }
+
+    func testPerformanceExample() throws {
+        self.measure { }
+    }
+
+}
+// swiftlint:enable overridden_super_call
---
+++ herohjk/Project/DoItSomedayUITests/DoItSomedayUITests.swift
@@ -0,0 +1,37 @@
+//
+//  DoItSomedayUITests.swift
+//  DoItSomedayUITests
+//
+//  Created by 김호종 on 2022/01/10.
+//
+
+import XCTest
+
+// swiftlint:disable overridden_super_call
+class DoItSomedayUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws { }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
+// swiftlint:enable overridden_super_call
